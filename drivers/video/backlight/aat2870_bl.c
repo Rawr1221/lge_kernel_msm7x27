cb83e7c895cdbdea0e2e915883177c46add10f92
Rawr1221 <kevin9876574@hotmail.com>
2013-01-11 18:57:43 -0800

    white noise

--- drivers/video/backlight/aat2870_bl.c
+++ drivers/video/backlight/aat2870_bl.c
@@ -421,6 +421,7 @@ EXPORT_SYMBOL(aat28xx_ldo_set_level);
 static void aat28xx_power_internal(struct aat28xx_driver_data *drvdata, int on)
 {
 #ifdef CONFIG_MACH_MSM7X27_THUNDERG
+    mdelay(20);
     return;
 #endif
 #ifdef CONFIG_MACH_MSM7X27_ALESSI
